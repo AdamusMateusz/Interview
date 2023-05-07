1e959f76cb93cdf1acaf8af71f88a74913fcef46
Adamus <adamus.mat@gmail.com>
2023-05-07 19:36:56 +0200

    Sending persintent messages to rabbit

--- src/main/java/com/cdq/interview/task/execution/sender/TasksExecutionService.java
+++ src/main/java/com/cdq/interview/task/execution/sender/TasksExecutionService.java
@@ -2,8 +2,10 @@ package com.cdq.interview.task.execution.sender;
 
 import com.cdq.interview.task.execution.mapper.MessageSerializer;
 import com.cdq.interview.task.execution.model.ExecuteTaskCommand;
+import com.rabbitmq.client.AMQP;
 import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.jms.JmsProperties;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
@@ -18,6 +20,10 @@ public class TasksExecutionService {
     private FluxSink<ExecuteTaskCommand> sink;
 
     public TasksExecutionService(Sender sender, MessageSerializer messageSerializer) {
+        AMQP.BasicProperties messageProperties = new AMQP.BasicProperties().builder()
+                .deliveryMode(JmsProperties.DeliveryMode.PERSISTENT.getValue())
+                .build();
+
         Flux<OutboundMessage> infiniteCommandsFlux =
                 Flux.<ExecuteTaskCommand>create(fluxSink -> this.sink = fluxSink)
                         .map(messageSerializer::fromCommandObject)
@@ -25,6 +31,7 @@ public class TasksExecutionService {
                                 new OutboundMessage(
                                         "interview.adamus",
                                         "task",
+                                        messageProperties,
                                         bytes
                                 )
                         );
